3c154431423942d46c7a972b0aa615a24f1d68ac
Jonyigit <jonyigitavazbekov@gmail.com>
2026-01-18 14:08:15 +0500

    finish build

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    root: "./",
-    build: {
-        outDir: "../dist",
-    },
-});
+  plugins: [react()],
+  build: {
+    outDir: 'dist', 
+  }
+})
